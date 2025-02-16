51c3f8574e951d6f3432ca3a58eb31eabf73d5c5
shpig <shpig@shpigs-MacBook-Pro.local>
2025-02-16 14:40:46 +0200

    remove navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,26 +27,6 @@ const addEventOnElements = function (elements, eventType, callback) {
   }
 }
 
-
-
-/**
- * NAVBAR
- */
-
-const navbar = document.querySelector("[data-navbar]");
-const navTogglers = document.querySelectorAll("[data-nav-toggler]");
-const overlay = document.querySelector("[data-overlay]");
-
-const toggleNavbar = function () {
-  navbar.classList.toggle("active");
-  overlay.classList.toggle("active");
-  document.body.classList.toggle("nav-active");
-}
-
-addEventOnElements(navTogglers, "click", toggleNavbar);
-
-
-
 /**
  * HEADER & BACK TOP BTN
  */
